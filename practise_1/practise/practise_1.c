261ad6563998544c23f7894ecae1af71f5a302ea
fxy <1442167451@qq.com>
2023-03-21 17:48:31 +0800

    在一个有序数组中查找具体的某个数字n

---
+++ practise_1/practise/practise_1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 6;//写一个代码，在arr数组（有序的）中找到6
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i < sz; i++)
+	{
+		if (k == arr[i])
+		{
+			printf("找到了，下标是:%d\n", i);
+			break;
+		}
+	}
+	if (i == sz)
+		printf("找不到\n");
+	return 0;
+}
